3e2b96da4f32094ab83314c0fb45daf12a1a76ef
AndPre <afpreciadoc@udistrital.edu.co>
2025-07-02 15:16:07 -0500

    Primer Commit (Creacion Proyectos)

---
+++ reddUD/src/main/java/edu/progAvUD/reddUD/ReddUdApplication.java
@@ -0,0 +1,13 @@
+package edu.progAvUD.reddUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddUdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReddUdApplication.class, args);
+	}
+
+}
---
+++ reddUD/src/test/java/edu/progAvUD/reddUD/ReddUdApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.progAvUD.reddUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddUdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
